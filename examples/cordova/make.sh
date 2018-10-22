9e00ea2d03207f6b1c0559072affd21a23cedaa3
Angel Jimenez <ajimenez@nayarsystems.com>
2018-10-22 17:22:37 +0200

    Added example with Cordova

---
+++ examples/cordova/make.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+deleteSplashImages() {
+    rm -f platforms/android/res/drawble*/screen.png
+}
+
+echoHeader() {
+    LINE="--$(echo $1|sed s/./-/g)--"
+    echo $LINE
+    echo "| $1 |"
+    echo $LINE
+}
+
+prerun() {
+    rm -rf cordova/www/
+    cp -r app cordova/www
+    npm run dev
+    cp ../../dist/multible.js cordova/www
+}
+
+run() {
+    prerun
+    pushd cordova > /dev/null
+    cordova run $1
+    popd > /dev/null
+}
+
+for arg in "$@"
+do
+    case $arg in
+     ra)
+          echoHeader "Running Android cordova project"
+          run android
+          ;;       
+    ri)
+          echoHeader "Running iOS cordova project"
+
+          prerun
+          pushd cordova > /dev/null
+          cordova prepare ios
+          cordova build ios --device
+          popd > /dev/null
+
+          xcodebuild -exportArchive -archivePath examples/cordova/cordova/platforms/ios/multible.xcarchive -exportPath /Volumes/tmp -exportOptionsPlist deploy/ios/exportOptions.plist
+          ;;    
+     li)
+          IPA="/tmp/multible.ipa"
+          touch $IPA
+          echoHeader "Listening for changes on $IPA for reinstall"
+          while [ true ]
+          do 
+            inotifywait -e close_write $IPA && ideviceinstaller -i $IPA 
+          done
+          ;;                       
+     *)
+          echoHeader "Unknown command: $arg"
+          exit
+          ;;             
+    esac
+done
